0eda9ab7578d7d98089b3546e23555600cdb9070
RomanTsarenko <czarenko.roman@vu.cdu.edu.ua>
2022-10-14 23:30:08 +0300

    Vectors:D

---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f;
+
+    void Start() 
+    {
+        //this.transform.Translate(goal);
+        //goal = goal * 0.01f;
+    }
+
+    //void Update() 
+    //{
+        //this.transform.Translate(goal.normalized * speed);
+    //}
+
+    void LateUpdate()
+    {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
